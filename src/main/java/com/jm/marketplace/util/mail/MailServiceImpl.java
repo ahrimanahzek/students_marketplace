ef0ec368986223355205d032e66949696ff68cd1
pro <ndg23@bk.ru>
2021-02-04 10:30:48 +0300

    create config class MailConfig, interface MailService, class MailServiceImpl

--- src/main/java/com/jm/marketplace/util/mail/MailServiceImpl.java
+++ src/main/java/com/jm/marketplace/util/mail/MailServiceImpl.java
@@ -3,11 +3,17 @@ package com.jm.marketplace.util.mail;
 import com.jm.marketplace.model.User;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collection;
 
 @Component
@@ -26,12 +32,32 @@ public class MailServiceImpl implements MailService {
         mailMessage.setText(message);
         mailMessage.setSubject(subject);
         mailMessage.setTo(user.getEmail());
-        javaMailSender.send(mailMessage);
+        try {
+            javaMailSender.send(mailMessage);
+        } catch (MailException e) {
+            throw new RuntimeException("", e);
+        }
     }
 
     @Override
     public void send(@NonNull User user, @NonNull String subject, @NonNull String message, @NonNull File... files) {
-
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setText(message);
+            helper.setSubject(subject);
+            helper.setTo(user.getEmail());
+            Arrays.stream(files).forEach(file -> {
+                try {
+                    helper.addAttachment(file.getName(), new FileSystemResource(file));
+                } catch (MessagingException e) {
+                    throw new RuntimeException("", e);
+                }
+            });
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException messageException) {
+            throw new RuntimeException("", messageException);
+        }
     }
 
     @Override
@@ -41,6 +67,6 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void broadcast(@NonNull Collection<User> users, @NonNull String subject, @NonNull String message, @NonNull File... files) {
-
+        users.forEach(user -> send(user, subject, message, files));
     }
 }
