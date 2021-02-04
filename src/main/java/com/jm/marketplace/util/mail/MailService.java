f2ed0fe0b128226a202a13632f3e2488e9b63f3e
pro <ndg23@bk.ru>
2021-02-04 10:18:59 +0300

    create config class MailConfig, interface MailService, class MailServiceImpl

--- src/main/java/com/jm/marketplace/util/mail/MailService.java
+++ src/main/java/com/jm/marketplace/util/mail/MailService.java
@@ -7,8 +7,8 @@ import java.util.Collection;
 
 public interface MailService {
 
-    void send(User user, String message);
-    void send(User user, String message, File... files);
-    void broadcast(Collection<User> users, String message);
-    void broadcast(Collection<User> users, String message, File... files);
+    void send(User user, String subject, String message);
+    void send(User user, String subject, String message, File... files);
+    void broadcast(Collection<User> users, String subject, String message);
+    void broadcast(Collection<User> users, String subject, String message, File... files);
 }
--- src/main/java/com/jm/marketplace/util/mail/MailServiceImpl.java
+++ src/main/java/com/jm/marketplace/util/mail/MailServiceImpl.java
@@ -1,6 +1,10 @@
 package com.jm.marketplace.util.mail;
 
 import com.jm.marketplace.model.User;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -9,23 +13,34 @@ import java.util.Collection;
 @Component
 public class MailServiceImpl implements MailService {
 
+    private final JavaMailSender javaMailSender;
+    private MailService mailService;
+    @Autowired
+    public MailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
     @Override
-    public void send(User user, String message) {
+    public void send(@NonNull User user, @NonNull String subject, @NonNull String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setText(message);
+        mailMessage.setSubject(subject);
+        mailMessage.setTo(user.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+
+    @Override
+    public void send(@NonNull User user, @NonNull String subject, @NonNull String message, @NonNull File... files) {
 
     }
 
     @Override
-    public void send(User user, String message, File... files) {
-
+    public void broadcast(@NonNull Collection<User> users, @NonNull String subject, @NonNull String message) {
+        users.forEach(user -> send(user, subject, message));
     }
 
     @Override
-    public void broadcast(Collection<User> users, String message) {
-
-    }
-
-    @Override
-    public void broadcast(Collection<User> users, String message, File... files) {
+    public void broadcast(@NonNull Collection<User> users, @NonNull String subject, @NonNull String message, @NonNull File... files) {
 
     }
 }
